b6945c726ac33df7e365977e67548d5e5d8e938d
aliolio21 <aliolio21@gmail.com>
2023-01-21 15:01:40 +0700

    add data course

--- ario_academy/__manifest__.py
+++ ario_academy/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "ario_academy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Training Odoo Academy from Arkana""",
 
     'description': """
-        Long description of module's purpose
+       Training Odoo Academy from Arkana
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Arkana",
+    'website': "http://www.arkana.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -27,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/course_data.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
