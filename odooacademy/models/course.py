68eae96bf2cf02991bc9e854b75a7b0d67ab013d
khaikalaz1@gmail.com <khaikalaz1@gmail.com>
2023-01-22 15:22:10 +0700

    Python Constraint

--- odooacademy/models/course.py
+++ odooacademy/models/course.py
@@ -6,6 +6,11 @@ class Course(models.Model):
     _name = 'odooacademy.course'
     _description = 'odooacademy.course'
 
+    _sql_constraints = [
+		('check_name_description_different','CHECK (name != description)','Name and description must be different'),
+		('check_name_unique','UNIQUE(name)','Name must be unique')
+	]
+    
     name = fields.Char('Title', required=True, index=True)
     user_id = fields.Many2one('res.users', string='Responsible User')
     session_ids = fields.One2many('odooacademy.session', 'course_id', string='Sessions')
