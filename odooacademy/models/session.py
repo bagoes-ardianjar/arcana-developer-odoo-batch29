85b60a0392af5dc9ff48873869bf098bbfda22c1
khaikalaz1@gmail.com <khaikalaz1@gmail.com>
2023-01-22 10:21:32 +0700

    Many2one

--- odooacademy/models/session.py
+++ odooacademy/models/session.py
@@ -7,6 +7,8 @@ class Session(models.Model):
     _description = 'odooacademy.session'
 
     name = fields.Char('Name', required=True, index=True)
+    course_id = fields.Many2one('odooacademy.course', string='Course')
+    user_id = fields.Many2one('res.users', string='Instructor')
     start_date = fields.Date('Start Date')
     duration = fields.Float('Duration')
     number_of_seats = fields.Float('Number Of Seats')
