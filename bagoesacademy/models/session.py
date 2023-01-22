9c7266b74abfe571449dec53cf66385839c60e4d
Bagoes Ardianjar <beck.ardian@gmail.com>
2023-01-22 10:32:16 +0700

    Many2one

--- bagoesacademy/models/session.py
+++ bagoesacademy/models/session.py
@@ -7,6 +7,8 @@ class Session(models.Model):
     _description = 'bagoesacademy.session'
 
     name = fields.Char('Name', required=True, index=True)
+    course_id = fields.Many2one('bagoesacademy.course', string='Course')
+    user_id = fields.Many2one('res.users', string='Instructor')
     start_date = fields.Date('Start Date')
     duration = fields.Float('Duration')
     number_of_seats = fields.Float('Number Of Seats')
