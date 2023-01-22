239b69d84b78e1680acadbd2fbeed946d583be2c
naufalnab <naufalnabila27@gmail.com>
2023-01-22 13:59:46 +0700

    define function di dalam models

--- naufalacademy/models/session.py
+++ naufalacademy/models/session.py
@@ -8,7 +8,7 @@ class Session(models.Model):
 
     name = fields.Char('Name', required=True, index=True)
     course_id = fields.Many2one('naufalacademy.course', string='Course')
-    # user_id = fields.Many2one('res.users', string='Instructor', domain="[('is_instructor','=',True),('is_teacher','=',True)]") 
+    # user_id = fields.Many2one('res.users', string='Instructor', domain="[('is_instructor','=',True),('is_teacher','=',True)]")
     # user_id = fields.Many2one('res.users', string='Instructor', domain="['|',('is_instructor','=',True),('is_teacher','=',True)]")
     user_id = fields.Many2one('res.users', string='Instructor', domain="[('is_instructor','=',True)]")
     partner_ids = fields.Many2many('res.partner', string='Attendees')
@@ -16,3 +16,12 @@ class Session(models.Model):
     duration = fields.Float('Duration')
     number_of_seats = fields.Float('Number Of Seats')
     description = fields.Text('Description')
+    taken_seats = fields.Float(string='Taken Seats', compute='_count_taken_seats', store=True)
+
+    @api.depends('partner_ids','number_of_seats')
+    def _count_taken_seats(self):
+        for rec in self:
+            if rec.partner_ids and rec.number_of_seats:
+                rec.taken_seats = len(rec.partner_ids) / rec.number_of_seats * 100
+            else:
+                rec.taken_seats = 0
