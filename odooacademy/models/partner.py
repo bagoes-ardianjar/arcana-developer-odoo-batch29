2abdcf43912caabe1ec9b76458748ef752e59ebc
khaikalaz1@gmail.com <khaikalaz1@gmail.com>
2023-01-22 11:37:56 +0700

    Inherits

---
+++ odooacademy/models/partner.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class InheritPartner(models.Model):
+    _inherit = 'res.partner'
+
+    session_ids = fields.Many2many('odooacademy.session', string='Sessions')
