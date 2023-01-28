3a9c7d8b19396e36b673410a014f31f6f57d0884
rifkialfian <rifki.deki.313@gmail.com>
2023-01-28 09:00:28 +0700

    Override copy method

--- rifkiacademy/models/course.py
+++ rifkiacademy/models/course.py
@@ -4,18 +4,48 @@ from odoo import models, fields, api
 
 
 class course(models.Model):
-    _name = 'rifkiacademy.course'
-    _description = 'rifkiacademy.course'
+	_name = 'rifkiacademy.course'
+	_description = 'rifkiacademy.course'
 
-    name = fields.Char('Title', required=True)
-    user_id = fields.Many2one('res.users', string='Responsible User')
-    session_ids = fields.One2many('rifkiacademy.session', 'course_id', string='Sessions')
-    description = fields.Text('Description')
+	_sql_constraints = [
+		('check_name_description_different','CHECK (name != description)','Name and description must be different'),
+		('check_name_unique','UNIQUE(name)','Name must be unique')
+	]
 
-    # value = fields.Integer()
-    # value2 = fields.Float(compute="_value_pc", store=True)
+	def copy(self, default=None):
+		# import pdb;
+		# pdb.set_trace()
+		# []
+		default = dict(default or {}) 
 
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     for record in self:
-    #         record.value2 = float(record.value) / 100
+		# Training Odoo
+		# Cari course name nya like Copy of Training odoo
+		# 3    
+		copied_count = self.search_count(
+			[('name', '=like', "Copy of {}%".format(self.name))])
+		
+		# Kalau tidak ada
+		if not copied_count:
+			# Copy of training odoo
+			new_name = "Copy of {}".format(self.name)
+		
+		# # Kalau ada
+		else:
+			# Copy of training odoo (jumlah ada berapa)
+			new_name = "Copy of {} ({})".format(self.name, copied_count)
+
+		default['name'] = new_name
+		return super(course, self).copy(default)
+
+	name = fields.Char('Title', required=True, index=True,)
+	user_id = fields.Many2one('res.users', string='Responsible User')
+	session_ids = fields.One2many('rifkiacademy.session', 'course_id', string='Sessions')
+	description = fields.Text('Description')
+
+	# value = fields.Integer()
+	# value2 = fields.Float(compute="_value_pc", store=True)
+
+	# @api.depends('value')
+	# def _value_pc(self):
+	#     for record in self:
+	#         record.value2 = float(record.value) / 100
