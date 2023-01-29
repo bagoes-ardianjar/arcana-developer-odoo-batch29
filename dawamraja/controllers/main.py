f04c914fc6a47bd6ba6f75735cf403d1b4c06024
Dawam Raja <akunnyadawam9@gmailcom>
2023-01-29 13:20:50 +0700

    err

--- dawamraja/controllers/main.py
+++ dawamraja/controllers/main.py
@@ -12,31 +12,31 @@ class Dawamraja(http.Controller):
         return http.request.render('dawamraja.teachers', {
             'teachers': ["Ujang", "Budi", "Dedi"],
         })
-    
+
     @http.route('/index/courses/', auth='public')
     def courses(self, **kw):
         courses = http.request.env['dawamraja.course'].sudo().search([])
         return http.request.render('dawamraja.courses', {
             'courses': courses,
         })
-    
+
     @http.route('/courses/', auth='public', website=True)
     def courses2(self, **kw):
-        courses = http.request.env['odooacademy.course'].sudo().search([])
-        return http.request.render('odooacademy.course_websites', {
+        courses = http.request.env['dawamraja.course'].sudo().search([])
+        return http.request.render('dawamraja.course_websites', {
             'courses': courses,
         })
     
-    @http.route('/courses/<model("odooacademy.course"):course>/', auth='public', website=True)
+    @http.route('/courses/<model("dawamraja.course"):course>/', auth='public', website=True)
     def course(self, course):
-        return http.request.render('odooacademy.course', {
+        return http.request.render('dawamraja.course', {
             'course': course
         })
     
     @http.route('/courses/add/', auth='public', website=True)
     def add(self, **kw):
         users = http.request.env['res.users'].sudo().search([])
-        return http.request.render('odooacademy.add_course', {
+        return http.request.render('dawamraja.add_course', {
             'users': users
         })
     
@@ -48,23 +48,41 @@ class Dawamraja(http.Controller):
                 'user_id': post.get('user_id'),
                 'description': post.get('description')
             }
-            http.request.env['odooacademy.course'].sudo().create(vals)
+            http.request.env['dawamraja.course'].sudo().create(vals)
         return http.request.redirect('/courses/')
 
-    @http.route('/courses/edit/<model("odooacademy.course"):course>/', auth='public', website=True)
-    def edit(self, course):
-        return http.request.render('odooacademy.edit_course', {
-            'course': course
+    @http.route('/courses/edit/<int:id>/', auth='public', website=True)
+    def edit(self, id):
+        if id:
+            course = http.request.env['dawamraja.course'].sudo().browse(int(id))
+        users = http.request.env['res.users'].sudo().search([])
+        return http.request.render('dawamraja.edit_course', {
+            'course': course,
+            'users': users
         })
+
+    @http.route('/course/do-edit/', auth='public', website=True, method='POST')
+    def do_edit(self, **post):
+        if post.get('course_id'):
+            course = http.request.env['dawamraja.course'].sudo().browse(int(post.get('course_id')))
+            if course:
+                vals = {
+                    'name' : post.get('name'),
+                    'user_id': post.get('user_id'),
+                    'description': post.get('description')
+                }
+                course.sudo().write(vals)
+        return http.request.redirect('/courses/')
     
     @http.route('/courses/delete/<int:id>/', auth='public', website=True)
     def delete(self, id):
         if id:
-            course = http.request.env['odooacademy.course'].sudo().browse(int(id))
+            course = http.request.env['dawamraja.course'].sudo().browse(int(id))
             if course:
                 course.unlink()
         return http.request.redirect('/courses/')
     
+
     @http.route('/url/<name>', auth='public', website=True)
     def url_name(self, name):
         return "<h1>{}</h1>".format(name)
@@ -72,15 +90,3 @@ class Dawamraja(http.Controller):
     @http.route('/type/<int:id>', auth='public', website=True)
     def url_type(self, id):
         return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
-    
-    @http.route('/courses/<model("dawamraja.course"):course>/', auth='public', website=True)
-    def course(self, course):
-        return http.request.render('dawamraja.course', {
-            'course': course
-        })
-
-
-
-
-
-
