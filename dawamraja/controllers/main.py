c04f492522d6f60a06dea546f85e16a9da0dd7d6
Dawam Raja <akunnyadawam9@gmailcom>
2023-01-29 10:19:35 +0700

    url

--- dawamraja/controllers/main.py
+++ dawamraja/controllers/main.py
@@ -26,6 +26,11 @@ class Dawamraja(http.Controller):
         return http.request.render('dawamraja.course_websites', {
             'courses': courses,
         })
+    
+    @http.route('/url/<name>', auth='public', website=True)
+    def url_name(self, name):
+        return "<h1>{}</h1>".format(name)
+
 
 
 
