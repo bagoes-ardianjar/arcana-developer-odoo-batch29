f46178ea04421ca80260d262fbc85f213a444f88
khaikalaz1@gmail.com <khaikalaz1@gmail.com>
2023-01-29 09:40:30 +0700

    Templates

--- odooacademy/controllers/main.py
+++ odooacademy/controllers/main.py
@@ -6,3 +6,9 @@ class Odooacademy(http.Controller):
     @http.route('/index', auth='public')
     def index(self, **kw):
         return "Hello, world"
+    
+    @http.route('/index/teachers/', auth='public')
+    def index(self, **kw):
+        return http.request.render('odooacademy.teachers', {
+            'teachers': ["Ujang", "Budi", "Dedi"],
+        })
