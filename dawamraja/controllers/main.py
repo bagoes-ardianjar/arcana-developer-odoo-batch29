eda6307ecddc3f434497b3b48176335a61fbde17
Dawam Raja <akunnyadawam9@gmailcom>
2023-01-29 11:07:20 +0700

    edit delete

--- dawamraja/controllers/main.py
+++ dawamraja/controllers/main.py
@@ -27,6 +27,24 @@ class Dawamraja(http.Controller):
             'courses': courses,
         })
     
+    @http.route('/courses/edit/<model("dawamraja.course"):course>/', auth='public', website=True)
+    def edit(self, course):
+        return http.request.render('dawamraja.edit_course', {
+            'course': course
+        })
+    
+    @http.route('/courses/delete/<int:id>/', auth='public', website=True)
+    def delete(self, id):
+        if id:
+            course = http.request.env['dawamraja.course'].sudo().browse(int(id))
+            if course:
+                course.unlink()
+        courses = http.request.env['dawamraja.course'].sudo().search([])
+        return http.request.render('dawamraja.course_websites', {
+            'courses': courses
+        })
+
+    
     @http.route('/url/<name>', auth='public', website=True)
     def url_name(self, name):
         return "<h1>{}</h1>".format(name)
